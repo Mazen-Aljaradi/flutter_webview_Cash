e6b7d14a16de1e8cbce7d034d788dcfe356aad4c
Mazen-Aljaradi <31169139+Mazen-Aljaradi@users.noreply.github.com>
2023-11-02 01:08:56 +0300

    new

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_webview_cash/FlutterWebviewPlugin.h>)
+#import <flutter_webview_cash/FlutterWebviewPlugin.h>
+#else
+@import flutter_webview_cash;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterWebviewPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterWebviewPlugin"]];
+}
+
+@end
